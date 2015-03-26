4a4993aa4cd7a2b185cf75f9a754ae03504212af
Eddie Krebs <ekrebs@gmail.com>
2015-03-26 15:47:06 -0400

    Tabs now work on mobile. Tab change now triggers the "tabChanged" event, passing the element and tabname.

--- dist/js/pwn.js
+++ dist/js/pwn.js
@@ -217,16 +217,36 @@
 
 		return this.each( function () {
 			var t = $( this );
+			var tb, ph, phc, tabName;
 
 			if ( 'true' === t.attr( 'data-pwned' ) ) {
 				return;
 			}
 
+			// Build accordian headers
+			t.find( '.tab' ).each( function() {
+				ph = $( this ).clone();
+				phc = ( ph.hasClass( 'active') ) ? "page-header active" : "page-header";
+				ph.attr( "class", phc );
+				ph.insertBefore( $( ".page[data-tab=" + ph.attr( 'data-tab' ) + "]") );
+			});
+
 			t.on( "click touchstart", ".tab", function( e ) {
+				// Tab click
 				e.preventDefault();
-				var tb = $( this );
+				tb = $( this );
+				tabName = tb.attr( 'data-tab' );
 				$( '.tab, .page', t ).removeClass( 'active' );
-				$( ".page[data-tab=" + tb.attr( 'data-tab' ) + "], .tab[data-tab=" + tb.attr( 'data-tab' ) + "]", t ).addClass( 'active' );
+				$( ".page[data-tab=" + tabName + "], .tab[data-tab=" + tabName + "]", t ).addClass( 'active' );
+				t.trigger( 'tabChanged', tabName );
+			}).on( "click touchstart", ".page-header", function(e) {
+				// Accordian click
+				e.preventDefault();
+				ph = $( this );
+				tabName = ph.attr( 'data-tab' );
+				$( '.page-header, .page', t ).removeClass( 'active' );
+				$( ".page[data-tab=" + tabName + "], .page-header[data-tab=" + tabName + "]", t ).addClass( 'active' );
+				t.trigger( 'tabChanged', tabName );
 			});
 
 			t.attr( 'data-pwned', 'true' );
