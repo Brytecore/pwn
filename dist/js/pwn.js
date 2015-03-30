641e50711c8935769e072ac2f82a3a202d8f8482
Eddie Krebs <ekrebs@gmail.com>
2015-03-30 11:12:18 -0400

    tabChanged event now only triggeres if selected tab wasn't active.

--- dist/js/pwn.js
+++ dist/js/pwn.js
@@ -219,11 +219,12 @@
 			var t = $( this );
 			var tb, ph, phc, tabName;
 
+			// Exit if this element has already had pwn_tabs run on it.
 			if ( 'true' === t.attr( 'data-pwned' ) ) {
 				return;
 			}
 
-			// Build accordian headers
+			// Build accordion headers
 			t.find( '.tab' ).each( function() {
 				ph = $( this ).clone();
 				phc = ( ph.hasClass( 'active') ) ? "page-header active" : "page-header";
@@ -231,24 +232,31 @@
 				ph.insertBefore( $( ".page[data-tab='" + ph.attr( 'data-tab' ) + "']") );
 			});
 
-			t.on( "click touchstart", ".tab", function( e ) {
-				// Tab click
+			// Change page on tab/accordion click
+			t.on( "click touchstart", ".tab, .page-header", function( e ) {
 				e.preventDefault();
+
 				tb = $( this );
+				if ( tb.hasClass( 'active' ) ) {
+					// Exit if current tab is already active
+					return;
+				}
+
+				// Determine which tab was clicked
 				tabName = tb.attr( 'data-tab' );
-				$( '.tab, .page', t ).removeClass( 'active' );
-				$( ".page[data-tab='" + tabName + "'], .tab[data-tab='" + tabName + "']", t ).addClass( 'active' );
-				t.trigger( 'tabChanged', tabName );
-			}).on( "click touchstart", ".page-header", function(e) {
-				// Accordian click
-				e.preventDefault();
-				ph = $( this );
-				tabName = ph.attr( 'data-tab' );
-				$( '.page-header, .page', t ).removeClass( 'active' );
-				$( ".page[data-tab='" + tabName + "'], .page-header[data-tab='" + tabName + "']", t ).addClass( 'active' );
+
+				// Remove active class from all pages, tabs and page-headers
+				$( '.tab, .page-header, .page', t ).removeClass( 'active' );
+
+				// Add active class to active page, tab and page-header
+				$( ".page[data-tab='" + tabName + "'], .tab[data-tab='" + tabName + "'], .page-header[data-tab='" + tabName + "']", t ).addClass( 'active' );
+
+				// Trigger the tabChanged event.
 				t.trigger( 'tabChanged', tabName );
 			});
 
+			// Track that this element has been altered, in order to prevent
+			// running this code again, even if user tries to.
 			t.attr( 'data-pwned', 'true' );
 		});
 	};
